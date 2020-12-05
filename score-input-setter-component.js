78e4660b0891c9277aa74a45a28f886f8592b98c
Thomas Barles <Paraintom@users.noreply.github.com>
2020-12-05 19:14:20 +0000

    Improvements
    - min max
    - bigger button
    - state saved in url

--- score-input-setter-component.js
+++ score-input-setter-component.js
@@ -9,17 +9,26 @@ var scoreInputSetterComponent = {
 		}
 	},
 	methods :{
-		show(context, title, defaultValue,step=1) {
+		show(context, title, defaultValue,step=1, max = 30) {
 			this.context = context;
 			this.title = title;
 			this.value = defaultValue;
 			this.step = step;
+			this.max = max;
 			this.isActive = true;
 		},
 		set(value = this.value) {
+			this.value = value > this.max ? this.max : value;
 			this.value = value;
 			this.isActive = false;
 			this.$emit('value-changed', this)
+		},
+		stepUpOrDown(isUp) {			
+			this.value = isUp ? this.value + this.step : this.value - this.step;
+			if(this.value <0)
+				this.value = 0;
+			if(this.value > this.max)
+				this.value = this.max;
 		}
 	},
 	template: `	
@@ -32,14 +41,14 @@ var scoreInputSetterComponent = {
 			</header>
 			<section class="modal-card-body">
 				<div class="field has-addons has-addons-centered">
-					<p class="control" @click="value-=step">
-						<a class="button"> - </a>
+					<p class="control" @click="stepUpOrDown(false)">
+						<a class="button" style="padding: 0px 2.5em;"> - </a>
 					</p>
 					<div class="control">
-						<input v-model="value" class="input has-text-centered" :placeholder="title" type="number" :step="step" min="0" max="30"/>
+						<input v-model="value" class="input has-text-centered" :placeholder="title" type="number" :step="step" min="0" :max="max"/>
 					</div>
-					<p class="control" @click="value+=step">
-						<a class="button"> + </a>
+					<p class="control" @click="stepUpOrDown(true)">
+						<a class="button" style="padding: 0px 2.5em;"> + </a>
 					</p>
 				</div>
 			</section>
